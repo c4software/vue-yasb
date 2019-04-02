f0c3abbfeead2bf3cad46bd63fbe6b0f7641e791
Valentin Brosseau <c4software@gmail.com>
2019-04-02 13:31:16 +0200

    Change title

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const debug = true;
+export const articlesFolder = "/articles/";
+
+// See https://router.vuejs.org/guide/essentials/history-mode.html#example-server-configurations
+export const html5HistoryMode = false;
