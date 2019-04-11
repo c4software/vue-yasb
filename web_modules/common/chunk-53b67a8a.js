a182748c0427227cab72a4578158e8dbcc6befba
Valentin Brosseau <c4software@gmail.com>
2019-04-11 08:59:56 +0200

    Switch to local web_modules dependancies

---
+++ web_modules/common/chunk-53b67a8a.js
@@ -0,0 +1,8 @@
+var commonjsGlobal = typeof globalThis !== 'undefined' ? globalThis : typeof window !== 'undefined' ? window : typeof global !== 'undefined' ? global : typeof self !== 'undefined' ? self : {};
+
+function createCommonjsModule(fn, module) {
+	return module = { exports: {} }, fn(module, module.exports), module.exports;
+}
+
+export { createCommonjsModule as a, commonjsGlobal as b };
+//# sourceMappingURL=chunk-53b67a8a.js.map
