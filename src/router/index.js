969394addfb890d15f294c2718d3786522eccc40
Valentin Brosseau <c4software@gmail.com>
2019-04-11 22:26:21 +0200

    Remove unnecessary process injection

--- src/router/index.js
+++ src/router/index.js
@@ -4,9 +4,6 @@ import ArticleLoader from "../views/articleLoader.js";
 import { html5HistoryMode, home } from "../config.js";
 import staticLoader from "../views/staticLoader.js";
 
-// Hacky way to make esm work with browser-esm
-window.process = { env: { NODE: "production" } };
-
 Vue.use(VueRouter);
 
 const routes = [{ path: "/", component: staticLoader, meta: { file: home } }, { path: "/:filename", component: ArticleLoader }];
